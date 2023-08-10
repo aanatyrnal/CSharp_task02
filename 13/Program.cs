82b577b0568a0de6eb098e1fe516722727b7831b
aanatyrnal <cacina_ang1891@mail.ru>
2023-08-10 23:33:50 +0300

    add tasks 13 and 15

---
+++ 13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num<100){
+    Console.WriteLine("третьей цифры нет");
+}
+else 
+{
+    int d = num / 100 ;
+    while(d > 10)
+    {
+        d = d % 10 ;
+    }
+    Console.WriteLine(d);
+}
---
+++ 15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num%6==0 || num%7==0){
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
